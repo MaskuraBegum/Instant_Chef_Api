e8b6482b7d0ef66387274543b8fbf955961fd847
MaskurBegum <maskura.begum77@gamil.com>
2025-02-19 00:36:36 +0600

    edited uid

---
+++ ~/vscode-safe-mode/User/History/-4b7fb6a5/8GVs.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    uid: { type: String, required: true, unique: true ,index: true},  // Store Firebase UID as String
+    email: { type: String, required: true, unique: true },  // Store Firebase Email
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'recipe' }],  // Reference to Recipe model
+});
+userSchema.index({ uid: 1 }, { unique: true });
+module.exports = mongoose.model('User', userSchema);
---
+++ ~/vscode-safe-mode/User/History/-4b7fb6a5/t41a.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    uid: { type: String, required: true, unique: true },  // Store Firebase UID as String
+    email: { type: String, required: true, unique: true },  // Store Firebase Email
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'recipe' }],  // Reference to Recipe model
+});
+
+module.exports = mongoose.model('User', userSchema);
