b534abada647f671e094d725d7887963491140a8
MaskurBegum <maskura.begum77@gamil.com>
2025-03-16 13:05:11 +0600

    Updated Firebase config to use Render Secret File

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/service-account-key.json');
+const serviceAccount = require('/etc/secrets/service-account-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
